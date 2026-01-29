4a72e0c39646484349e279e379f881190d4a9e36
Caio <pess.caio@hotmail.com>
2026-01-29 15:34:01 -0300

    initial commit

---
+++ src/components/icon.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+export const iconVariants = cva("", {
+  variants: {
+    animate: {
+      false: "",
+      true: "animate-spin",
+    },
+  },
+  defaultVariants: {
+    animate: false,
+  },
+});
+
+// Pega as propiedades do svg e coloca dentro da interface
+interface IconProps 
+  extends React.ComponentProps<"svg">,
+    VariantProps<typeof iconVariants> {
+  svg: React.FC<React.ComponentProps<"svg">>;
+}
+
+export default function Icon({svg: SvgComponent, animate, className, ...props}: IconProps) {
+  return <SvgComponent className={iconVariants({animate, className})} {...props} />
+}
